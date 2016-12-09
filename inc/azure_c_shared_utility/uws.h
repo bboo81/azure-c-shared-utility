6620cf1a614fe834f08511c19ccd0db80cb9d5b6
Dan Cristoloveanu <dcristo@microsoft.com>
2016-12-08 19:50:45 -0800

    Add uws

---
+++ inc/azure_c_shared_utility/uws.h
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#ifndef UWS_H
+#define UWS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct UWS_INSTANCE_TAG* UWS_HANDLE;
+
+extern UWS_HANDLE uws_create();
+extern void uws_destroy(UWS_HANDLE ws_io);
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif /* UWS_H */
